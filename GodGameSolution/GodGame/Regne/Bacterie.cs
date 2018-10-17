bcafe2dbc8c970c436efbd86319a1bfee7a4c1ff
Mourier Héloïse <Mourier Hlose>
2018-10-17 17:59:42 +0200

    Creation Classe

--- GodGameSolution/GodGame/Regne/Bacterie.cs
+++ GodGameSolution/GodGame/Regne/Bacterie.cs
@@ -3,12 +3,28 @@ namespace GodGame.Regne
 {
     public class Bacterie
     {
-        private String typeName;
-        private byte populationNumber;
-        private String description;
 
-        public Bacterie()
+        private byte populationNumber;
+        private String name;
+
+        public string Name { get => name; set => name = value; }
+
+        public Bacterie(String p_name)
         {
+            this.Name = p_name; 
+
+        }
+
+        enum EspeceBacterie
+        {
+            Champignon,
+            Archee,
+            Protiste,
+        }
+
+        public void TosTring()
+        {
+            Console.WriteLine("Je m'appelle {0} , je suis de race {1} et de regne {3}");
         }
     }
 }
--- GodGameSolution/GodGame/Regne/Vegetal.cs
+++ GodGameSolution/GodGame/Regne/Vegetal.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 namespace GodGame.Regne
 {
-    public class Vegetal
+    public class Vegetal 
     {
-        private String typeName;
-        private byte populationNumber;
-        private String description;
 
-        public Vegetal()
+        private byte populationNumber;
+        private String name;
+
+        public string Name { get => name; set => name = value; }
+
+        public Vegetal(String p_name)
         {
+            this.Name = p_name; 
+        }
+
+        enum EspeceVegetal
+        {
+            Arbre,
+            Fleur,
+            Buisson, 
+        }
+
+        public void TosTring()
+        {
+            Console.WriteLine("Je m'appelle {0} , je suis de race {1} , et de Regne  {3} ");
         }
     }
 }
